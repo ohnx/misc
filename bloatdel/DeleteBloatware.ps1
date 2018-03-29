eb31ef8628b2ef01ecd4fb31a5e3253cae0be3dd
Mason <masonx.01@gmail.com>
2018-03-28 20:49:36 -0400

    Update DeleteBloatware.ps1

--- bloatdel/DeleteBloatware.ps1
+++ bloatdel/DeleteBloatware.ps1
@@ -1,8 +1,8 @@
 # list of names from https://www.howtogeek.com/224798/how-to-uninstall-windows-10s-built-in-apps-and-how-to-reinstall-them/
 
-$bloat0 = "*3dbuilder*","*windowsalarms*","*windowscalculator*","*windowscommunicationsapps*","*windowscamera*","*officehub*","*skypeapp*"
+$bloat0 = "*3dbuilder*","*windowsalarms*","*windowscalculator*","*messaging*","*windowscamera*","*officehub*","*skypeapp*"
 $bloat1 = "*getstarted*","*zunemusic*","*windowsmaps*","*solitairecollection*","*bingfinance*","*zunevideo*","*bingnews*","*onenote*"
-$bloat2 = "*people*","*phone*","*photos*","*windowsstore*","*bingsports*","*soundrecorder*","*bingweather*","*xboxapp*"
+$bloat2 = "*people*","*phone*","*photos*","*windowsstore*","*bingsports*","*soundrecorder*","*bingweather*","*sway*","*xboxapp*"
 
 $bloats = $bloat0 + $bloat1 + $bloat2
 
