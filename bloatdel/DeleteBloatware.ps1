ebc26ba2d1434e273946d4e7860de71059af9416
Mason <masonx.01@gmail.com>
2018-03-28 20:41:15 -0400

    Create DeleteBloatware.ps1

---
+++ bloatdel/DeleteBloatware.ps1
@@ -0,0 +1,12 @@
+# list of names from https://www.howtogeek.com/224798/how-to-uninstall-windows-10s-built-in-apps-and-how-to-reinstall-them/
+
+$bloat0 = "*3dbuilder*","*windowsalarms*","*windowscalculator*","*windowscommunicationsapps*","*windowscamera*","*officehub*","*skypeapp*"
+$bloat1 = "*getstarted*","*zunemusic*","*windowsmaps*","*solitairecollection*","*bingfinance*","*zunevideo*","*bingnews*","*onenote*"
+$bloat2 = "*people*","*phone*","*photos*","*windowsstore*","*bingsports*","*soundrecorder*","*bingweather*","*xboxapp*"
+
+$bloats = $bloat0 + $bloat1 + $bloat2
+
+Foreach ($bloat in $bloats)
+{
+  Get-AppxPackage $bloat | Remove-AppxPackage
+}
