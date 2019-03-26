7cd868eb5f67ef5bc2f841e08ba9a1738c5a57ac
ohnx <me@masonx.ca>
2019-03-25 21:58:51 -0400

    updates

--- jinglepings/README.md
+++ jinglepings/README.md
@@ -1,1 +1,8 @@
 # tools for https://jinglepings.com
+
+more help:
+https://github.com/dgibson/iputils/blob/master/ping_common.c
+https://github.com/dgibson/iputils/blob/master/ping.c
+https://github.com/dgibson/iputils
+https://tools.ietf.org/html/rfc2460#section-8.2
+https://tools.ietf.org/html/rfc2463#section-2.3
--- jinglepings/color-flood.c
+++ jinglepings/color-flood.c
@@ -10,6 +10,59 @@
 #include <arpa/inet.h>
 #include <ifaddrs.h>
 
+void hexDump (char *desc, void *addr, int len) {
+    int i;
+    unsigned char buff[17];
+    unsigned char *pc = (unsigned char*)addr;
+
+    // Output description if given.
+    if (desc != NULL)
+        printf ("%s:\n", desc);
+
+    if (len == 0) {
+        printf("  ZERO LENGTH\n");
+        return;
+    }
+    if (len < 0) {
+        printf("  NEGATIVE LENGTH: %i\n",len);
+        return;
+    }
+
+    // Process every byte in the data.
+    for (i = 0; i < len; i++) {
+        // Multiple of 16 means new line (with line offset).
+
+        if ((i % 16) == 0) {
+            // Just don't print ASCII for the zeroth line.
+            if (i != 0)
+                printf ("  %s\n", buff);
+
+            // Output the offset.
+            printf ("  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+        printf (" %02x", pc[i]);
+
+        // And store a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+
+    // Pad out last line if not exactly 16 characters.
+    while ((i % 16) != 0) {
+        printf ("   ");
+        i++;
+    }
+
+    // And print the final ASCII bit.
+    printf ("  %s\n", buff);
+}
+
+
 void color2ip(char *rgb_hex, struct in6_addr *addr) {
     /* this is the ip to use for 2018 - see https://jinglepings.com */
     char ip[] = {'2','0','0','1',':','4','c','0','8',':','2','0','2','8',':','2','0','1','9',':',':','0','0',':','0','0',':','0','0',0x0};
@@ -298,6 +351,8 @@ int main(int argc, char **argv) {
         icmp_header->checksum = 0;
         *icmp_pseudoheader = *icmp_header;
         *icmp_pseudopayload = *icmp_payload;
+        hexDump("packet", packet, packet_len);
+        hexDump("checksum", checksum, checksum_len);
         icmp_header->checksum = icmpv6_calc_checksum(checksum, checksum_len); /* htons is not necessary since the checksum function does it already */
 
         /* done, we have a fully formed packet! time to send it... */
